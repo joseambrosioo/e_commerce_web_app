305568b2f3d241c5bca34d6156ede88d6a2f8c05
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-09-19 15:47:16 -0500

    Continue implemening product image upload to the add product form 04
    Added products are now being saved to the database.

--- client/src/pages/admin-view/products.jsx
+++ client/src/pages/admin-view/products.jsx
@@ -3,6 +3,7 @@ import CommonForm from "@/components/common/form";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
 import { addProductFormElements } from "@/config";
+import { useToast } from "@/hooks/use-toast";
 import { addNewProduct, fetchAllProducts } from "@/store/admin/products-slice";
 import { Description } from "@radix-ui/react-toast";
 
@@ -22,13 +23,14 @@ const initialFormData = {
 
 function AdminProducts() {
 
-    const [openCreateProductsDialog, setOpencreateProductsDialog] = useState(false);
+    const [openCreateProductsDialog, setOpenCreateProductsDialog] = useState(false);
     const [formData, setFormData] = useState(initialFormData)
     const [imageFile, setImageFile] = useState(null);
     const [uploadedImageUrl, setUploadedImageUrl] = useState('');
     const [imageLoadingState, setImageLoadingState] = useState(false);
     const { productList } = useSelector(state => state.adminProducts)
     const dispatch = useDispatch();
+    const { toast } = useToast();
 
     function onSubmit(event) {
         event.preventDefault();
@@ -38,6 +40,16 @@ function AdminProducts() {
         })
         ).then((data) => {
             console.log(data);
+            if (data?.payload?.success) {
+                dispatch(fetchAllProducts())
+                setOpenCreateProductsDialog(false)
+                setImageFile(null);
+                setFormData(initialFormData);
+                toast({
+                    title: 'Product added successfully'
+
+                })
+            }
         });
     }
 
@@ -49,13 +61,13 @@ function AdminProducts() {
 
     return <Fragment>
         <div className="mb-5 w-full flex justify-end">
-            <Button onClick={() => setOpencreateProductsDialog(true)} className="bg-black text-white font-semibold">Add New Product</Button>
+            <Button onClick={() => setOpenCreateProductsDialog(true)} className="bg-black text-white font-semibold">Add New Product</Button>
         </div>
         <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
         <Sheet
             open={openCreateProductsDialog}
             onOpenChange={() => {
-                setOpencreateProductsDialog(false);
+                setOpenCreateProductsDialog(false);
             }}
         >
             <SheetContent side="right" className="overflow-auto bg-white text-black p-5 shadow-lg max-h-full">

91a7f661857ae7f3a4ed185f50f97adb658f2654
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-09-29 23:28:13 -0500

    Implement Logout functionality for the Admin View 01
    Implement Logout functionality for the Admin View

--- client/src/components/admin-view/header.jsx
+++ client/src/components/admin-view/header.jsx
@@ -1,16 +1,18 @@
+import { logoutUser } from "@/store/auth-slice";
 import { Button } from "../ui/button";
 import { AlignJustify } from "lucide-react";
 import { LogOut } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 function AdminHeader({ setOpen }) {
     const navigate = useNavigate();
 
+    const dispatch = useDispatch()
 
-    const handleLogout = () => {
-        // Implement your logout logic here
-        navigate('/auth/login');
-    };
+    function handleLogout() {
+        dispatch(logoutUser());
+    }
 
     return (
         <header className="flex items-cent justify-between px-4 py-3 bg-backgraound border-b">
@@ -19,7 +21,7 @@ function AdminHeader({ setOpen }) {
                 <span className="sr-only">Toggle Menu</span>
             </Button>
             <div className="flex flex-1 justify-end"></div>
-            <Button className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow bg-black text-white font-semibold">
+            <Button onClick={handleLogout} className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow bg-black text-white font-semibold">
                 {/* <Button className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow"> */}
                 <LogOut />
                 Logout
--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -34,12 +34,27 @@ export const loginUser = createAsyncThunk('/auth/login',
     }
 );
 
+export const logoutUser = createAsyncThunk('/auth/logout',
+
+    async () => {
+        const response = await axios.post(
+            'http://localhost:5000/api/auth/logout',
+            {},
+            {
+                withCredentials: true,
+            }
+        );
+
+        return response.data;
+    }
+);
+
 export const checkAuth = createAsyncThunk('/auth/checkauth',
     async () => {
         const response = await axios.get('http://localhost:5000/api/auth/check-auth',
             {
                 withCredentials: true,
-                header: {
+                headers: {
                     'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
                     Expires: '0'
                 }
@@ -58,41 +73,56 @@ const authSlice = createSlice({
         setUser: (state, action) => { },
     },
     extraReducers: (builder) => {
-        builder.addCase(registerUser.pending, (state) => {
-            state.isLoading = true;
-        }).addCase(registerUser.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.user = null;
-            state.isAuthenticated = false;
-        }).addCase(registerUser.rejected, (state, action) => {
-            state.isLoading = false;
-            state.user = null;
-            state.isAuthenticated = false;
-        }).addCase(loginUser.pending, (state) => {
-            state.isLoading = true;
-        }).addCase(loginUser.fulfilled, (state, action) => {
-            console.log(action);
-            state.isLoading = false;
-            state.user = action.payload.success ? action.payload.user : null;
-            state.isAuthenticated = action.payload.success
-            // state.isAuthenticated = action.payload.success ? true : false;
-        }).addCase(loginUser.rejected, (state, action) => {
-            state.isLoading = false;
-            state.user = null;
-            state.isAuthenticated = false;
-        }).addCase(checkAuth.pending, (state) => {
-            state.isLoading = true;
-        }).addCase(checkAuth.fulfilled, (state, action) => {
-            console.log(action);
-            state.isLoading = false;
-            state.user = action.payload.success ? action.payload.user : null;
-            state.isAuthenticated = action.payload.success;
-            // state.isAuthenticated = action.payload.success ? true : false;
-        }).addCase(checkAuth.rejected, (state, action) => {
-            state.isLoading = false;
-            state.user = null;
-            state.isAuthenticated = false;
-        });
+        builder
+            .addCase(registerUser.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(registerUser.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.user = null;
+                state.isAuthenticated = false;
+            })
+            .addCase(registerUser.rejected, (state, action) => {
+                state.isLoading = false;
+                state.user = null;
+                state.isAuthenticated = false;
+            })
+            .addCase(loginUser.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(loginUser.fulfilled, (state, action) => {
+                console.log(action);
+                state.isLoading = false;
+                state.user = action.payload.success ? action.payload.user : null;
+                state.isAuthenticated = action.payload.success
+                // state.isAuthenticated = action.payload.success ? true : false;
+            })
+            .addCase(loginUser.rejected, (state, action) => {
+                state.isLoading = false;
+                state.user = null;
+                state.isAuthenticated = false;
+            })
+            .addCase(checkAuth.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(checkAuth.fulfilled, (state, action) => {
+                console.log(action);
+                state.isLoading = false;
+                state.user = action.payload.success ? action.payload.user : null;
+                state.isAuthenticated = action.payload.success;
+                // state.isAuthenticated = action.payload.success ? true : false;
+            })
+            .addCase(checkAuth.rejected, (state, action) => {
+                state.isLoading = false;
+                state.user = null;
+                state.isAuthenticated = false;
+            })
+            .addCase(logoutUser.fulfilled, (state, action) => {
+                // console.log(action);
+                state.isLoading = false;
+                state.user = null;
+                state.isAuthenticated = false;
+            });
     },
 });
 

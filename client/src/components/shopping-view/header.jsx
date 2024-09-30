88228d2a76b0c3f6a107feaec5d3b6771d4a02ee
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-09-29 23:43:44 -0500

    Implement header and products listing for the user/shopping view 01
    Start implementing header for the user/shopping view

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -1,7 +1,15 @@
+import { HousePlug } from "lucide-react";
+import { Link } from "react-router-dom";
+
 function ShoppingHeader() {
-    return (
-        <div>shopping view header</div>
-    );
+    return <header className="sticky top-0 z-40 w-full border-b bg-background">
+        <div className="flex h-16 item-center justify-between px-4 md:px-6">
+            <Link className="flex">
+                <HousePlug className="h-6 w-6" />
+                <span>E-commerce</span>
+            </Link>
+        </div>
+    </header >
 }
 
 export default ShoppingHeader;

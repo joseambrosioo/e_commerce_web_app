2175e8704e144b825b45bb7aec154d30078ed888
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-10-06 23:12:04 -0500

    Implement products listing for the customer view 01
    Start implementing products listing for the customer view

--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -1,5 +1,13 @@
+import ProductFilter from "@/components/shopping-view/filter";
+
 function ShoppingListing() {
-    return <div>shopping view listing</div>
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6">
+            <ProductFilter>
+
+            </ProductFilter>
+        </div>
+    );
 }
 
 export default ShoppingListing;

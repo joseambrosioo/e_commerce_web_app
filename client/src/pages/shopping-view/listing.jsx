843c02b79666c0ab287d5ca38eef2e83e0774489
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-10-06 23:46:40 -0500

    Implement products listing for the customer view 05
    Continue implementing products listing for the customer view

--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -8,9 +8,9 @@ function ShoppingListing() {
         <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6">
             <ProductFilter />
             <div className="bg-background w-full rounded-lg shadow-sm">
-                <div className="p-4 border-b flex gap-3 items-center justify-between">
+                <div className="p-4 border-b flex items-center justify-between">
                     <h2 className="text-lg font-extrabold mr-2">All Products</h2>
-                    <div className="flex items-center gap-2">
+                    <div className="flex items-center gap-3">
                         <span className="text-muted-foreground">10 Products</span>
                         <DropdownMenu>
                             <DropdownMenuTrigger asChild>

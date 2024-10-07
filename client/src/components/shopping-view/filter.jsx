874d3a15fcf4a397317e8c413ab8410df7a3c043
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-10-06 23:27:00 -0500

    Implement products listing for the customer view 02
    Continue implementing products listing for the customer view

--- client/src/components/shopping-view/filter.jsx
+++ client/src/components/shopping-view/filter.jsx
@@ -2,6 +2,7 @@ import { filterOptions } from "@/config";
 import { Fragment } from "react";
 import { Label } from "../ui/label";
 import { Checkbox } from "../ui/checkbox";
+import { Separator } from "../ui/separator";
 
 function ProductFilter() {
     return (
@@ -11,23 +12,24 @@ function ProductFilter() {
             </div>
             <div className="p-4 space-y-4">
                 {
-                    Object.keys(filterOptions).map(keyItem =>
+                    Object.keys(filterOptions).map(keyItem => (
                         <Fragment>
                             <div>
                                 <h3 className="text-base font-bold">{keyItem}</h3>
                                 <div className="grid gap-2 mt-2">
                                     {
-                                        filterOptions[keyItem].map(option =>
-                                            <Label className="flex font-medium items-center gap-2 font-normal">
+                                        filterOptions[keyItem].map(option => (
+                                            <Label className="flex font-medium items-center gap-2">
                                                 <Checkbox />
                                                 {option.label}
                                             </Label>
-                                        )
+                                        ))
                                     }
                                 </div>
                             </div>
+                            <Separator />
                         </Fragment>
-                    )
+                    ))
                 }
             </div>
         </div >

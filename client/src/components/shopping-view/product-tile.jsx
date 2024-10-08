cf972699fde49029f5807ace56dae533e6d0f37a
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-10-07 23:41:19 -0500

    Implement products listing for the customer view 11
    Continue implementing products listing for the customer view

--- client/src/components/shopping-view/product-tile.jsx
+++ client/src/components/shopping-view/product-tile.jsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardFooter } from "../ui/card";
 import { Button } from "../ui/button";
 import { Badge } from "../ui/badge";
+import { brandOptionsMap, categoryOptionsMap } from "@/config";
 
 function ShoppingProductTile({ product }) {
     return (
@@ -20,17 +21,26 @@ function ShoppingProductTile({ product }) {
                 <CardContent className="p-4">
                     <h2 className="text-xl font-bold mb-2">{product?.title}</h2>
                     <div className="flex justify-between items-center mb-2">
-                        <span className="text-sm text-muted-foreground"> {product?.category}</span>
-                        <span className="text-sm text-muted-foreground"> {product?.brand}</span>
+                        <span className="text-sm text-muted-foreground">
+                            {/* {product?.category} */}
+                            {categoryOptionsMap[product?.category]}
+                        </span>
+                        <span className="text-sm text-muted-foreground">
+                            {/* {product?.brand} */}
+                            {brandOptionsMap[product?.brand]}
+                        </span>
                     </div>
                     <div className="flex justify-between items-center mb-2">
-                        <span className="{`${product?.salePrice > 0 ? 'line-through' : ''} text-lg font-semibold text-primary`}">{product?.price}
-                            {
-                                product?.salePrice > 0 ? (
-                                    <span className="text-lg font-semibold text-primary">{product?.salePrice}</span>
-                                ) : null
-                            }
+                        <span className={`${product?.salePrice > 0 ? 'line-through' : ''} text-lg font-semibold text-primary`}>
+                            ${product?.price}
                         </span>
+                        {
+                            product?.salePrice > 0 ? (
+                                <span className="text-lg font-semibold text-primary">
+                                    ${product?.salePrice}
+                                </span>
+                            ) : null
+                        }
                     </div>
                 </CardContent>
                 <CardFooter>

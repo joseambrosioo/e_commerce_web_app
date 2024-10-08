7c809a75a91727553151adbd5076ddcfc51b41d3
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-10-07 23:54:55 -0500

    Implement products listing for the customer view 12
    Continue implementing products listing for the customer view

--- client/src/components/shopping-view/product-tile.jsx
+++ client/src/components/shopping-view/product-tile.jsx
@@ -21,11 +21,11 @@ function ShoppingProductTile({ product }) {
                 <CardContent className="p-4">
                     <h2 className="text-xl font-bold mb-2">{product?.title}</h2>
                     <div className="flex justify-between items-center mb-2">
-                        <span className="text-sm text-muted-foreground">
+                        <span className="text-[16px] text-muted-foreground">
                             {/* {product?.category} */}
                             {categoryOptionsMap[product?.category]}
                         </span>
-                        <span className="text-sm text-muted-foreground">
+                        <span className="text-[16px] text-muted-foreground">
                             {/* {product?.brand} */}
                             {brandOptionsMap[product?.brand]}
                         </span>

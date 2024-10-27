ce70b4ba0f471f3b24fce90a6fc556bb468eca65
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-10-27 07:29:11 -0500

    Implement cart 03
    Continue implementing the cart functionality backend

---
+++ server/routes/shop/cart-routes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const {
+  addToCart,
+  fetchCartItems,
+  updateCartItemQuantity,
+  deleteCartItem,
+} = require("../../controllers/shop/cart-controller");
+
+const router = express.Ruoter();
+
+router.post("/add", addToCart);
+router.get("/get/:userId", fetchCartItems);
+router.put("/update-cart", updateCartItemQuantity);
+router.delete("/:userId/:productId", deleteCartItem);
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,31 +1,36 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const authRouter = require('./routes/auth/auth-routes');
-const adminProductsRouter = require('./routes/admin/products-routes')
-const shopProductsRouter = require('./routes/shop/products-routes')
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const authRouter = require("./routes/auth/auth-routes");
+const adminProductsRouter = require("./routes/admin/products-routes");
+const shopProductsRouter = require("./routes/shop/products-routes");
+const shopCartRouter = require("./routes/shop/cart-routes");
 
-mongoose.connect('mongodb+srv://jose-ambrosio:KUdtFQDBm37sDUwN@jose-ambrosio.ke25r.mongodb.net/').then(() => console.log('MongoDB connected')).catch(error => console.log);
+mongoose
+  .connect(
+    "mongodb+srv://jose-ambrosio:KUdtFQDBm37sDUwN@jose-ambrosio.ke25r.mongodb.net/"
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log);
 
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(
-    cors({
-        origin: 'http://localhost:5173',
-        methods: ['GET', 'POST', 'DELETE', 'PUT'],
-        allowedHeaders: [
-            "Content-Type",
-            'Authorization',
-            'Cache-Control',
-            'Expires',
-            'Pragma'
-        ],
-        credentials: true
-
-    })
-)
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
 
 // app.get('/', (req, res) => {
 //     res.send('Hello World!');
@@ -33,8 +38,9 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
-app.use('/api/auth', authRouter);
-app.use('/api/admin/products', adminProductsRouter);
-app.use('/api/shop/products', shopProductsRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/admin/products", adminProductsRouter);
+app.use("/api/shop/products", shopProductsRouter);
+app.use("/api/shop/cart", shopCartRouter);
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));

729357f750d4931dd8fcd4e4297dbd744bc34016
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-10-27 08:03:14 -0500

    Implement cart 02
    Start implementing the cart frontend

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,15 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './auth-slice'
-import adminProductsSlice from './admin/products-slice';
-import shopProductsSlice from './shop/products-slice';
+import { createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./auth-slice";
+import adminProductsSlice from "./admin/products-slice";
+import shopProductsSlice from "./shop/products-slice";
+import shopCartSlice from "./shop/cart-slice";
 
 const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        adminProducts: adminProductsSlice,
-        shopProducts: shopProductsSlice,
-    },
+  reducer: {
+    auth: authReducer,
+    adminProducts: adminProductsSlice,
+    shopProducts: shopProductsSlice,
+    shopCart: shopCartSlice,
+  },
 });
 
 export default store;

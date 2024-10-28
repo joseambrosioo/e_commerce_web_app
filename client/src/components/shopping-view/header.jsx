bf70cdead550d48cdcfa2ad1d30fa539f6289dcc
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-10-27 23:56:24 -0500

    Implement the homepage for the customer view 04
    Continue implementing the homepage for the customer view

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -1,5 +1,10 @@
 import { HousePlug, LogOut, Menu, ShoppingCart, UserRound } from "lucide-react";
-import { Link, useNavigate } from "react-router-dom";
+import {
+  Link,
+  useLocation,
+  useNavigate,
+  useSearchParams,
+} from "react-router-dom";
 import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 import { Button } from "../ui/button";
 import { useDispatch, useSelector } from "react-redux";
@@ -17,18 +22,44 @@ import { logoutUser } from "@/store/auth-slice";
 import UserCartWrapper from "./cart-wrapper";
 import { useEffect, useState } from "react";
 import { fetchCartItems } from "@/store/shop/cart-slice";
+import { Label } from "@radix-ui/react-label";
 
 function MenuItems() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  function handleNavigate(getCurrentMenuItem) {
+    sessionStorage.removeItem("filters");
+    const currentFilter =
+      getCurrentMenuItem.id !== "home" &&
+      getCurrentMenuItem.id !== "products" &&
+      getCurrentMenuItem.id !== "search"
+        ? {
+            category: [getCurrentMenuItem.id],
+          }
+        : null;
+
+    sessionStorage.setItem("filters", JSON.stringify(currentFilter));
+
+    location.pathname.includes("listing") && currentFilter !== null
+      ? setSearchParams(
+          new URLSearchParams(`?category=${getCurrentMenuItem.id}`)
+        )
+      : navigate(getCurrentMenuItem.path);
+  }
+
   return (
     <nav className="flex flex-col mb-3 lg:mb-0 lg:items-center gap-6 lg:flex-row">
       {shoppingViewHeaderMenuItems.map((menuItem) => (
-        <Link
-          className="text-sm font-medium"
+        <Label
+          onClick={() => handleNavigate(menuItem)}
+          className="text-sm font-medium cursor-pointer"
           key={menuItem.id}
-          to={menuItem.path}
+          // to={menuItem.path}
         >
           {menuItem.label}
-        </Link>
+        </Label>
       ))}
     </nav>
   );
--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,182 +1,180 @@
 export const registerFormControls = [
-    {
-        name: 'userName',
-        label: 'User Name',
-        placeholder: 'Enter your user name',
-        componentType: 'input',
-        type: 'text',
-    },
-    {
-        name: 'email',
-        label: 'Email',
-        placeholder: 'Enter your email',
-        componentType: 'input',
-        type: 'email',
-    },
-    {
-        name: 'password',
-        label: 'Password',
-        placeholder: 'Enter your password',
-        componentType: 'input',
-        type: 'password',
-    }
-]
+  {
+    name: "userName",
+    label: "User Name",
+    placeholder: "Enter your user name",
+    componentType: "input",
+    type: "text",
+  },
+  {
+    name: "email",
+    label: "Email",
+    placeholder: "Enter your email",
+    componentType: "input",
+    type: "email",
+  },
+  {
+    name: "password",
+    label: "Password",
+    placeholder: "Enter your password",
+    componentType: "input",
+    type: "password",
+  },
+];
 
 export const loginFormControls = [
-    {
-        name: 'email',
-        label: 'Email',
-        placeholder: 'Enter your email',
-        componentType: 'input',
-        type: 'email',
-    },
-    {
-        name: 'password',
-        label: 'Password',
-        placeholder: 'Enter your password',
-        componentType: 'input',
-        type: 'password',
-    }
+  {
+    name: "email",
+    label: "Email",
+    placeholder: "Enter your email",
+    componentType: "input",
+    type: "email",
+  },
+  {
+    name: "password",
+    label: "Password",
+    placeholder: "Enter your password",
+    componentType: "input",
+    type: "password",
+  },
 ];
 
 export const addProductFormElements = [
-    {
-        label: 'Title',
-        name: 'title',
-        componentType: 'input',
-        type: 'text',
-        placeholder: 'Enter product title',
-    },
-    {
-        label: 'Description',
-        name: 'description',
-        componentType: 'textarea',
-        placeholder: 'Enter product descripton',
-        type: 'password',
-    },
-    {
-        label: 'Category',
-        name: 'category',
-        componentType: 'select',
-        placeholder: 'Select a category',
-        options: [
-            { id: "men", label: "Men" },
-            { id: "women", label: "Women" },
-            { id: "kids", label: "Kids" },
-            { id: "accessories", label: "Accessories" },
-            { id: "footwear", label: "Footwear" },
-        ]
-    },
-    {
-        label: 'Brand',
-        name: 'brand',
-        componentType: 'select',
-        placeholder: 'Select a brand',
-        options: [
-            { id: "nike", label: "Nike" },
-            { id: "adidas", label: "Adidas" },
-            { id: "puma", label: "Puma" },
-            { id: "levi", label: "Levi" },
-            { id: "zara", label: "Zara" },
-            { id: "h&m", label: "H&M" },
-        ]
-    },
-    {
-        label: 'Price',
-        name: 'price',
-        componentType: 'input',
-        type: 'number',
-        placeholder: 'Enter product price',
-    },
-    {
-        label: 'Sale Price',
-        name: 'salePrice',
-        componentType: 'input',
-        type: 'number',
-        placeholder: 'Enter product sale price (optional)',
-    },
-    {
-        label: 'Total Stock',
-        name: 'totalStock',
-        componentType: 'input',
-        type: 'number',
-        placeholder: 'Enter total stock',
-    }
+  {
+    label: "Title",
+    name: "title",
+    componentType: "input",
+    type: "text",
+    placeholder: "Enter product title",
+  },
+  {
+    label: "Description",
+    name: "description",
+    componentType: "textarea",
+    placeholder: "Enter product descripton",
+    type: "password",
+  },
+  {
+    label: "Category",
+    name: "category",
+    componentType: "select",
+    placeholder: "Select a category",
+    options: [
+      { id: "men", label: "Men" },
+      { id: "women", label: "Women" },
+      { id: "kids", label: "Kids" },
+      { id: "accessories", label: "Accessories" },
+      { id: "footwear", label: "Footwear" },
+    ],
+  },
+  {
+    label: "Brand",
+    name: "brand",
+    componentType: "select",
+    placeholder: "Select a brand",
+    options: [
+      { id: "nike", label: "Nike" },
+      { id: "adidas", label: "Adidas" },
+      { id: "puma", label: "Puma" },
+      { id: "levi", label: "Levi" },
+      { id: "zara", label: "Zara" },
+      { id: "h&m", label: "H&M" },
+    ],
+  },
+  {
+    label: "Price",
+    name: "price",
+    componentType: "input",
+    type: "number",
+    placeholder: "Enter product price",
+  },
+  {
+    label: "Sale Price",
+    name: "salePrice",
+    componentType: "input",
+    type: "number",
+    placeholder: "Enter product sale price (optional)",
+  },
+  {
+    label: "Total Stock",
+    name: "totalStock",
+    componentType: "input",
+    type: "number",
+    placeholder: "Enter total stock",
+  },
 ];
 
 export const shoppingViewHeaderMenuItems = [
-    {
-        id: 'home',
-        label: 'Home',
-        path: '/shop/home'
-    },
-    {
-        id: 'men',
-        label: 'Men',
-        path: '/shop/listing'
-    },
-    {
-        id: 'women',
-        label: 'Women',
-        path: '/shop/listing'
-    },
-    {
-        id: 'kids',
-        label: 'Kids',
-        path: '/shop/listing'
-    },
-    // {
-    //     id: 'watch',
-    //     label: 'Watch',
-    //     path: '/shop/listing'
-    // },
-    // {
-    //     id: 'accessories',
-    //     label: 'Accessories',
-    //     path: '/shop/listing'
-    // },
-]
+  {
+    id: "home",
+    label: "Home",
+    path: "/shop/home",
+  },
+  {
+    id: "men",
+    label: "Men",
+    path: "/shop/listing",
+  },
+  {
+    id: "women",
+    label: "Women",
+    path: "/shop/listing",
+  },
+  {
+    id: "kids",
+    label: "Kids",
+    path: "/shop/listing",
+  },
+  // {
+  //     id: 'watch',
+  //     label: 'Watch',
+  //     path: '/shop/listing'
+  // },
+  // {
+  //     id: 'accessories',
+  //     label: 'Accessories',
+  //     path: '/shop/listing'
+  // },
+];
 
 export const categoryOptionsMap = {
-    men: "Men",
-    women: "Women",
-    kids: "Kids",
-    accessories: "Accessories",
-    footwear: "Footwear",
+  men: "Men",
+  women: "Women",
+  kids: "Kids",
+  accessories: "Accessories",
+  footwear: "Footwear",
 };
 
 export const brandOptionsMap = {
-    nike: "Nike",
-    adidas: "Adidas",
-    puma: "Puma",
-    levi: "Levi",
-    zara: "Zara",
-    "h&m": "H&M",
+  nike: "Nike",
+  adidas: "Adidas",
+  puma: "Puma",
+  levi: "Levi",
+  zara: "Zara",
+  "h&m": "H&M",
 };
-
 
 export const filterOptions = {
-    category: [
-        { id: "men", label: "Men" },
-        { id: "women", label: "Women" },
-        { id: "kids", label: "Kids" },
-        // { id: "accessories", label: "Accessories" },
-        // { id: "footwear", label: "Footwear" },
-    ],
-    brand: [
-        { id: "nike", label: "Nike" },
-        { id: "adidas", label: "Adidas" },
-        { id: "puma", label: "Puma" },
-        { id: "levi", label: "Levi" },
-        { id: "zara", label: "Zara" },
-        { id: "h&m", label: "H&M" },
-    ],
+  category: [
+    { id: "men", label: "Men" },
+    { id: "women", label: "Women" },
+    { id: "kids", label: "Kids" },
+    // { id: "accessories", label: "Accessories" },
+    // { id: "footwear", label: "Footwear" },
+  ],
+  brand: [
+    { id: "nike", label: "Nike" },
+    { id: "adidas", label: "Adidas" },
+    { id: "puma", label: "Puma" },
+    { id: "levi", label: "Levi" },
+    { id: "zara", label: "Zara" },
+    { id: "h&m", label: "H&M" },
+  ],
 };
 
-
 export const sortOptions = [
-    { id: "price-lowtohigh", label: "Price: Low to High" },
-    { id: "price-hightolow", label: "Price: High to Low" },
-    { id: "title-atoz", label: "Title: A to Z" },
-    { id: "title-ztoa", label: "Title: Z to A" },
+  { id: "price-lowtohigh", label: "Price: Low to High" },
+  { id: "price-hightolow", label: "Price: High to Low" },
+  { id: "title-atoz", label: "Title: A to Z" },
+  { id: "title-ztoa", label: "Title: Z to A" },
 ];

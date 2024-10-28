978f7b2cf42599cba0ffb0e1ef1daf2d44507dab
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-10-27 21:27:49 -0500

    Implement cart 10
    Continue implementing the cart frontend

--- client/src/components/shopping-view/cart-items-content.jsx
+++ client/src/components/shopping-view/cart-items-content.jsx
@@ -7,19 +7,29 @@ function UserCartItemsContent({ cartItem }) {
       <img
         sr={cartItem?.image}
         alt={cartItem?.title}
-        className="w-20 h-29 rounded object-cover"
+        className="w-20 h-20 rounded object-cover"
       />
       <div className="flex-1">
         <h3 className="font-extrabold">{cartItem?.title}</h3>
-        <div className="flex items-center mt-1"></div>
-        <Button variant="outline" className="h-8 w-8 rounded-full" size="icon">
-          <Minus className="w-4 h-4" />
-          <span className="sr-only">Decrease</span>
-        </Button>
-        <Button variant="outline" className="h-8 w-8 rounded-full" size="icon">
-          <Plus className="w-4 h-4" />
-          <span className="sr-only">Decrease</span>
-        </Button>
+        <div className="flex items-center gap-2 mt-1">
+          <Button
+            variant="outline"
+            className="h-8 w-8 rounded-full"
+            size="icon"
+          >
+            <Minus className="w-4 h-4" />
+            <span className="sr-only">Decrease</span>
+          </Button>
+          <span className="font-semibold">{cartItem?.quantity}</span>
+          <Button
+            variant="outline"
+            className="h-8 w-8 rounded-full"
+            size="icon"
+          >
+            <Plus className="w-4 h-4" />
+            <span className="sr-only">Decrease</span>
+          </Button>
+        </div>
       </div>
     </div>
   );

efd5bf274419f5a124c42e546465f71976e9e1f9
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-12-17 22:49:00 -0800

    Update server.js
    Updated server.js

--- server/server.js
+++ server/server.js
@@ -62,10 +62,10 @@ app.use("/api/shop/order", shopOrderRouter);
 app.use("/api/shop/search", shopSearchRouter);
 app.use("/api/shop/review", shopReviewRouter);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));

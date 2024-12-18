ce100fa5ebd72d01dafdb57f44fdc51c9bfa0e37
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-12-17 19:36:26 -0800

    Update tailwind.config.js
    Updated tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,6 @@
 // tailwind.config.js
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -19,5 +21,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };

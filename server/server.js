4718a2d7b01a9e272241f369f004e43ee1005e85
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-12-18 11:08:21 -0800

    Update server.js
    Updated server.js

--- server/server.js
+++ server/server.js
@@ -32,7 +32,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://e-commerce-web-app-q0k6.onrender.com",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",

ce3bfa111008604c78b10e5c5fb15aa2a9469436
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-12-18 17:48:58 -0800

    Create connectToMongoDB.js and update server.js
    Created connectToMongoDB.js and updated server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const path = require("path");
 const express = require("express");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
@@ -16,14 +16,15 @@ const shopAddressRouter = require("./routes/shop/address-routes");
 const shopOrderRouter = require("./routes/shop/order-routes");
 const shopSearchRouter = require("./routes/shop/search-routes");
 const shopReviewRouter = require("./routes/shop/review-routes");
+const connectToMongoDB = require("./helpers/connectToMongoDB");
 
-mongoose
-  .connect(process.env.MONGO_DB_URI)
-  // .connect(
-  //   "mongodb+srv://jose-ambrosio:KUdtFQDBm37sDUwN@jose-ambrosio.ke25r.mongodb.net/"
-  // )
-  .then(() => console.log("MongoDB connected"))
-  .catch((error) => console.log);
+// mongoose
+//   .connect(process.env.MONGO_DB_URI)
+//   // .connect(
+//   //   "mongodb+srv://jose-ambrosio:KUdtFQDBm37sDUwN@jose-ambrosio.ke25r.mongodb.net/"
+//   // )
+//   .then(() => console.log("MongoDB connected"))
+//   .catch((error) => console.log);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -70,4 +71,9 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
-app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server is now running on port ${PORT}`);
+});

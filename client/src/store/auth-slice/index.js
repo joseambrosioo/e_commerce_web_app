571a10114b2908619d01ecb823c5aec0e67e9940
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-12-18 18:21:51 -0800

    Update auth-slice/index.js and shop/order-controller.js
    Updated auth-slice/index.js and shop/order-controller.js

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -1,129 +1,131 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import axios from 'axios';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
-    isAuthenticated: false,
-    isLoading: true,
-    // isLoading: false,
-    user: null
+  isAuthenticated: false,
+  isLoading: true,
+  // isLoading: false,
+  user: null,
 };
 
-export const registerUser = createAsyncThunk('/auth/register',
-    async (formData) => {
-        const response = await axios.post('http://localhost:5000/api/auth/register',
-            formData,
-            {
-                withCredentials: true
-            }
-        );
+export const registerUser = createAsyncThunk(
+  "/auth/register",
+  async (formData) => {
+    const response = await axios.post(
+      "http://localhost:5000/api/auth/register",
+      formData,
+      {
+        withCredentials: true,
+      }
+    );
 
-        return response.data;
-    }
+    return response.data;
+  }
 );
 
-export const loginUser = createAsyncThunk('/auth/login',
-    async (formData) => {
-        const response = await axios.post('http://localhost:5000/api/auth/login',
-            formData,
-            {
-                withCredentials: true
-            }
-        );
-
-        return response.data;
+export const loginUser = createAsyncThunk("/auth/login", async (formData) => {
+  const response = await axios.post(
+    // "http://localhost:5000/api/auth/login",
+    "https://e-commerce-web-app-q0k6.onrender.com/auth/login",
+    formData,
+    {
+      withCredentials: true,
     }
+  );
+
+  return response.data;
+});
+
+export const logoutUser = createAsyncThunk(
+  "/auth/logout",
+
+  async () => {
+    const response = await axios.post(
+      "http://localhost:5000/api/auth/logout",
+      {},
+      {
+        withCredentials: true,
+      }
+    );
+
+    return response.data;
+  }
 );
 
-export const logoutUser = createAsyncThunk('/auth/logout',
-
-    async () => {
-        const response = await axios.post(
-            'http://localhost:5000/api/auth/logout',
-            {},
-            {
-                withCredentials: true,
-            }
-        );
-
-        return response.data;
+export const checkAuth = createAsyncThunk("/auth/checkauth", async () => {
+  const response = await axios.get(
+    "http://localhost:5000/api/auth/check-auth",
+    {
+      withCredentials: true,
+      headers: {
+        "Cache-Control":
+          "no-store, no-cache, must-revalidate, proxy-revalidate",
+        Expires: "0",
+      },
     }
-);
+  );
 
-export const checkAuth = createAsyncThunk('/auth/checkauth',
-    async () => {
-        const response = await axios.get('http://localhost:5000/api/auth/check-auth',
-            {
-                withCredentials: true,
-                headers: {
-                    'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
-                    Expires: '0'
-                }
-            }
-        );
-
-        return response.data;
-    }
-);
-
+  return response.data;
+});
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        setUser: (state, action) => { },
-    },
-    extraReducers: (builder) => {
-        builder
-            .addCase(registerUser.pending, (state) => {
-                state.isLoading = true;
-            })
-            .addCase(registerUser.fulfilled, (state, action) => {
-                state.isLoading = false;
-                state.user = null;
-                state.isAuthenticated = false;
-            })
-            .addCase(registerUser.rejected, (state, action) => {
-                state.isLoading = false;
-                state.user = null;
-                state.isAuthenticated = false;
-            })
-            .addCase(loginUser.pending, (state) => {
-                state.isLoading = true;
-            })
-            .addCase(loginUser.fulfilled, (state, action) => {
-                // console.log(action);
-                state.isLoading = false;
-                state.user = action.payload.success ? action.payload.user : null;
-                state.isAuthenticated = action.payload.success
-                // state.isAuthenticated = action.payload.success ? true : false;
-            })
-            .addCase(loginUser.rejected, (state, action) => {
-                state.isLoading = false;
-                state.user = null;
-                state.isAuthenticated = false;
-            })
-            .addCase(checkAuth.pending, (state) => {
-                state.isLoading = true;
-            })
-            .addCase(checkAuth.fulfilled, (state, action) => {
-                // console.log(action);
-                state.isLoading = false;
-                state.user = action.payload.success ? action.payload.user : null;
-                state.isAuthenticated = action.payload.success;
-                // state.isAuthenticated = action.payload.success ? true : false;
-            })
-            .addCase(checkAuth.rejected, (state, action) => {
-                state.isLoading = false;
-                state.user = null;
-                state.isAuthenticated = false;
-            })
-            .addCase(logoutUser.fulfilled, (state, action) => {
-                // console.log(action);
-                state.isLoading = false;
-                state.user = null;
-                state.isAuthenticated = false;
-            });
-    },
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {},
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        // console.log(action);
+        state.isLoading = false;
+        state.user = action.payload.success ? action.payload.user : null;
+        state.isAuthenticated = action.payload.success;
+        // state.isAuthenticated = action.payload.success ? true : false;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(checkAuth.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(checkAuth.fulfilled, (state, action) => {
+        // console.log(action);
+        state.isLoading = false;
+        state.user = action.payload.success ? action.payload.user : null;
+        state.isAuthenticated = action.payload.success;
+        // state.isAuthenticated = action.payload.success ? true : false;
+      })
+      .addCase(checkAuth.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(logoutUser.fulfilled, (state, action) => {
+        // console.log(action);
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      });
+  },
 });
 
 export const { setUser } = authSlice.actions;
--- server/controllers/shop/order-controller.js
+++ server/controllers/shop/order-controller.js
@@ -26,10 +26,8 @@ const createOrder = async (req, res) => {
       redirect_urls: {
         // return_url: "http://localhost:5173/shop/paypal-return",
         // cancel_url: "http://localhost:5173/shop/paypal-cancel",
-        return_url:
-          "https://e-commerce-web-app-q0k6.onrender.com/shop/paypal-return",
-        cancel_url:
-          "https://e-commerce-web-app-q0k6.onrender.com/paypal-cancel",
+        return_url: "http://localhost:5000/shop/paypal-return",
+        cancel_url: "http://localhost:5000/paypal-cancel",
       },
       transactions: [
         {
